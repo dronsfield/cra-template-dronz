b7207f2067dbf05863edbf4465797d04663e53f3
Josh Dronsfield <josh.dronsfield@gmail.com>
2021-08-11 17:47:58 +0100

    update sortBy

--- template/src/util/sortBy.ts
+++ template/src/util/sortBy.ts
@@ -3,9 +3,9 @@ function isEmpty(value: any) {
 }
 
 function compareFactory<T>(opts: {
-  prop: keyof T
+  prop?: keyof T
   desc?: boolean
-  valueTransform: (x: T[keyof T]) => any
+  valueTransform: (x: any) => any
   secondaryProp?: keyof T
 }): (left: T, right: T) => number {
   const { prop, desc, valueTransform, secondaryProp } = opts
@@ -15,16 +15,18 @@ function compareFactory<T>(opts: {
     : () => 0
 
   return (left: T, right: T) => {
+    const leftProp = prop ? left[prop] : left
+    const rightProp = prop ? right[prop] : right
     let order = 0
-    if (isEmpty(left[prop]) && isEmpty(right[prop])) {
+    if (isEmpty(leftProp) && isEmpty(rightProp)) {
       order = secondaryCompare(left, right)
-    } else if (isEmpty(left[prop])) {
+    } else if (isEmpty(leftProp)) {
       order = 1
-    } else if (isEmpty(right[prop])) {
+    } else if (isEmpty(rightProp)) {
       order = -1
     } else {
-      const l = valueTransform(left[prop])
-      const r = valueTransform(right[prop])
+      const l = valueTransform(leftProp)
+      const r = valueTransform(rightProp)
       if (l > r) {
         order = directionTransform(1)
       } else if (l < r) {
@@ -39,16 +41,19 @@ function compareFactory<T>(opts: {
 
 export function sortBy<T>(
   input: T[],
-  prop: keyof T,
+  prop?: keyof T,
   desc?: boolean,
   opts?: {
     castLower?: boolean
     secondaryProp?: keyof T
-    valueTransform?: (x: T[keyof T]) => any
+    valueTransform?: (x: any) => any
   }
 ) {
-  const { castLower, secondaryProp, valueTransform: valueTransformProp } =
-    opts || {}
+  const {
+    castLower,
+    secondaryProp,
+    valueTransform: valueTransformProp
+  } = opts || {}
   const valueTransform =
     valueTransformProp || castLower
       ? (x: T[keyof T]) => (typeof x === "string" ? x.toLowerCase() : x)
